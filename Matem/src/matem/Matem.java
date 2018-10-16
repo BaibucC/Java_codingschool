6c6f8df1df61b1379e243ec4948893fc94e2b385
BaibucC <44113149+BaibucC@users.noreply.github.com>
2018-10-16 16:02:48 +0300

    16.10.

---
+++ Matem/src/matem/Matem.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package matem;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Baiba
+ */
+public class Matem {
+
+    public static int rezultats(int num, int pak) {
+        // int rez = num;
+        if (pak <= 1) {
+            return num;
+        } else {
+            for (int i = pak; i > 0; i--) {
+                int rez = num * num;
+                i--;
+
+                rez = rez * num;
+                return rez;
+
+            }
+        }
+
+        return num;
+    }
+    
+    /*Math.pow(2, 4);
+this mean 2 to the power 4 (2^4)
+answer = 16 */
+    
+    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Skaitlis: ");
+        int num = sc.nextInt();
+        System.out.println("Kādā pakāpē? ");
+        int pak = sc.nextInt();
+        int rez = (int) Math.pow(num, pak);
+
+        System.out.println("Skaitlis '" + num + "' " + pak + ".pakāpē ir: " + rez);
+    }
+
+}
