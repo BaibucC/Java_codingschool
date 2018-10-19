08381412a123128f4305d9eaead533fcd8188950
BaibucC <44113149+BaibucC@users.noreply.github.com>
2018-10-20 01:59:05 +0300

    upd

---
+++ Strings/src/meg/Strings.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package meg;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Baiba
+ */
+public class Strings {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ievadiet vārdu: ");
+        Scanner sc = new Scanner(System.in);
+        String vards = sc.nextLine();
+
+        //String sadalīšana pa burtiem
+        char[] array = vards.toCharArray();
+        System.out.println(array);
+
+        for (int i = 0; i < vards.length(); i++) {
+            System.out.println(array[i]);
+        }
+
+        //apgriešana
+        String vards1 = "    zebiekste   ";
+        System.out.println("Pirms apgriešanas " + vards1);
+        System.out.println("Pēc apgriešans: " + vards1.trim());
+
+        String message = String.join("*", "Šodien", "ir", "labs", "laiks + " + vards + " + " + vards1.trim());
+        System.out.println(message);
+
+    }
+
+}
